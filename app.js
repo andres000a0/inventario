94b5feda0fdbdb1be8c90640caeaadc5ea9166a2
andres000a0 <andresmuri1999@gmail.com>
2023-02-22 00:28:15 -0500

    visual del login1

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const dropdowns = document.getElementById('.input-field')
+const dropdowns = document.querySelectorAll(".input-field");
 
-dropdowns.forEach(dropdown => {
-  const select = dropdown.querySelector('.select')
-  const caret = dropdown.querySelector('.caret')
-  const menu = dropdown.querySelector('.menu')
-  const selected = dropdown.querySelector('.selected')
+dropdowns.forEach((dropdown) => {
+  const selecte = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const selected = dropdown.querySelector(".selected");
+  const opciones = dropdown.querySelectorAll(".menu li");
 
-  select.addEventListener('click', ()=>{
-    select.class
-  })
-})
+  document.addEventListener("click", function() {
+    selecte.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
 
+  });
 
+  opciones.forEach((opcion) => {
+    opcion.addEventListener("click", () => {
+      selected.innerText = opcion.innerText;
+      selecte.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+
+      opciones.forEach((opcion) => {
+        opcion.classList.remove("active");
+      });
+      opcion.classList.add("active");
+    });
+  });
+});
